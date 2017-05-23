a02e6bf36c831e3a7029d69fbfeaa510ef76e7be
Precious Opusunju <haxorpreshy@gmail.com>
2017-05-23 02:54:05 +0100

    Initial Commit

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import CommentBox from './CommentBox';
+import style from './style';
+ReactDOM.render(
+  <CommentBox />,
+  document.getElementById('root')
+);
